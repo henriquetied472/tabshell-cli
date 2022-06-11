d620b8e863c1f6ddf3f35e33e5d8e39f8c6825af
Henrique Goulart Ferreira <hgoulart659@gmail.com>
2022-06-10 23:32:20 -0300

    api: make request and append the data in a file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/henriquetied472/tabshell-cli/api"
+)
+
+var debugger log.Logger
+var logger log.Logger
+
+func main() {
+	dgb := flag.Bool("debug", false, "Append the log file")
+	flag.Parse()
+
+	out, _ := os.OpenFile("./logs/latest.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	defer out.Close()
+	debugger = *log.Default()
+	logger = *log.Default()
+	debugger.SetOutput(out)
+
+	debugger.SetPrefix("DEBUG: ")
+	debugger.Printf("TabShell Initialized!!")
+	logger.Printf("Tabshell Initialized!!")
+
+	defer logger.Printf("TabShell Finished!!")
+	
+	api.Init(&debugger, &logger, dgb)
+}
